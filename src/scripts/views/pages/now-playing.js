7434fa289896f3b907ab8f53bc74b490533fecab
Farhan <101630148+frhan18@users.noreply.github.com>
2023-01-26 20:32:53 +0700

    membuat Application Shell & konfigurasi API www.themoviedb.org

--- src/scripts/views/pages/now-playing.js
+++ src/scripts/views/pages/now-playing.js
@@ -1,12 +1,27 @@
+/* eslint-disable array-callback-return */
+/* eslint-disable no-unused-vars */
+import TheMovieDbSource from '../../data/themoviedb-source';
+
+import { createMovieItemTemplate } from '../templates/template-creator';
+
 const NowPlaying = {
   async render() {
     return `
-      <h2>Now Playing Page</h2>
+      <div class="content">
+        <h2 class="content__heading">Now Playing in Cinema</h2>
+        <div id="movies" class="movies">
+        </div>
+      </div>
     `;
   },
 
   async afterRender() {
-    // Fungsi ini akan dipanggil setelah render()
+    const movies = await TheMovieDbSource.nowPlayingMovies();
+    const moviesContainer = document.querySelector('#movies');
+
+    movies.map((movie) => {
+      moviesContainer.innerHTML += createMovieItemTemplate(movie);
+    });
   },
 };
 
