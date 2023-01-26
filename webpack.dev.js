c79f528190cd76a6acfe0758eab3dbcdbe2c17ad
Farhan <101630148+frhan18@users.noreply.github.com>
2023-01-26 18:51:23 +0700

    Konfigurasi Webpack

---
+++ webpack.dev.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const webConfig = require('./webpack.config');
+
+module.exports = merge(webConfig, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    open: true,
+    port: 9000,
+    client: {
+      overlay: {
+        errors: true,
+        warnings: true,
+      },
+    },
+    compress: true,
+  },
+});
---
+++ webpack.prod.js
@@ -0,0 +1,23 @@
+const { merge } = require('webpack-merge');
+const webConfig = require('./webpack.config');
+
+module.exports = merge(webConfig, {
+  mode: 'production',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        ],
+      },
+    ],
+  },
+});
