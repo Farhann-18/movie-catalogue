5b30802e8e0cf4304d9e17c86315a74716851dd6
Farhan <101630148+frhan18@users.noreply.github.com>
2023-01-27 09:41:41 +0700

    Menerapkan service worker & cache

---
+++ src/scripts/utils/sw-register.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+const swRegister = async () => {
+  if (!('serviceWorker' in navigator)) {
+    console.log('Service Worker not supported in the browser');
+  }
+
+  try {
+    await navigator.serviceWorker.register('./sw.bundle.js');
+    // console.log('Service worker registered');
+  } catch (error) {
+    console.log('Failed to register service worker', error);
+  }
+};
+
+export default swRegister;
